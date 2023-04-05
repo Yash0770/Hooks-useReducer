b33e147dc9189da1bb23e0a7f50d51938ef737c6
Yash <100015221+Yash0770@users.noreply.github.com>
2023-04-05 18:20:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CounterTwo from './Componenet/CountTwo';
+// import CountOne from './Componenet/CountOne';
+
+function App() {
+  return (
+    <div className="App">
+          <CounterTwo/>
+    </div>
+  );
+}
+
+export default App;
